15339f9e027aab9807ce10df919b298c3cc9428f
sdelafuente <coalsan@gmail.com>
2021-04-19 23:13:05 -0300

    1.Saber cuánto espacio ocupa el total de las publicaciones de un usuario.

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -14,3 +14,13 @@ class Foto(val alto: Int, val ancho: Int) : Publicacion() {
 class Texto(val contenido: String) : Publicacion() {
   override fun espacioQueOcupa() = contenido.length
 }
+
+class Video(val duracion: Int, var calidad: Int=512) : Publicacion() {
+  override fun espacioQueOcupa(): Int {
+    when(calidad) {
+        720 -> return duracion * 3
+        1080 -> return duracion * 3 * 2
+        else -> return duracion
+    }
+  }
+}
--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -7,6 +7,7 @@ class UsuarioTest : DescribeSpec({
   describe("Caralibro") {
     val saludoCumpleanios = Texto("Felicidades Pepito, que los cumplas muy feliz")
     val fotoEnCuzco = Foto(768, 1024)
+    val videoEnLaMontania = Video(60)
 
     describe("Una publicación") {
       describe("de tipo foto") {
@@ -20,6 +21,20 @@ class UsuarioTest : DescribeSpec({
           saludoCumpleanios.espacioQueOcupa().shouldBe(45)
         }
       }
+
+      describe("de tipo video") {
+        it("ocupa tantos bytes como su duracion SD") {
+          videoEnLaMontania.espacioQueOcupa().shouldBe(60)
+        }
+        it("ocupa tantos bytes como su duracion por 3") {
+          videoEnLaMontania.calidad = 720
+          videoEnLaMontania.espacioQueOcupa().shouldBe(180)
+        }
+        it("ocupa tantos bytes como su duracion por el doble de 720") {
+          videoEnLaMontania.calidad = 1080
+          videoEnLaMontania.espacioQueOcupa().shouldBe(360)
+        }
+      }
     }
 
     describe("Un usuario") {
@@ -27,7 +42,8 @@ class UsuarioTest : DescribeSpec({
         val juana = Usuario()
         juana.agregarPublicacion(fotoEnCuzco)
         juana.agregarPublicacion(saludoCumpleanios)
-        juana.espacioDePublicaciones().shouldBe(550548)
+        juana.agregarPublicacion(videoEnLaMontania)
+        juana.espacioDePublicaciones().shouldBe(550608)
       }
     }
   }
